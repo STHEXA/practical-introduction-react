6a9e74a754e9a5dc2a7542ffb742f37aa60a1209
STHEXA <ti.syu.s@gmail.com>
2026-03-22 16:33:33 +0900

    イベントに応じての指定方法を確認

--- samples/my-react/src/main.jsx
+++ samples/my-react/src/main.jsx
@@ -582,11 +582,9 @@ const root = createRoot(document.getElementById("root"));
 //   <EmotionProps />
 // );
 
-root.render(<MotionBasic />);
+// root.render(<MotionBasic />);
 
-// root.render(
-//   <MotionWhile />
-// );
+root.render(<MotionWhile />);
 
 // root.render(
 //   <MotionExit />
